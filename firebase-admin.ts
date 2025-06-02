2309bc346ca372181968166153f783c3e1d5ddbc
Talha Tayyab <qararhanzala@gmail.com>
2025-06-02 17:34:25 +0500

    Switched Firebase admin config to use environment variables

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -1,17 +1,19 @@
-import { getApp, getApps, App, cert , initializeApp , } from "firebase-admin/app";
-
-
+import { getApp, getApps, App, cert, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-const servicekey = require ("./service_key.json");
-let app : App ;
-if (getApps().length=== 0) {
-    app = initializeApp({
-        credential : cert (servicekey)
-    });
-    
-}else{
-    app = getApp();
 
+let app: App;
+
+if (!getApps().length) {
+  app = initializeApp({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
+  });
+} else {
+  app = getApp();
 }
+
 const adminDb = getFirestore(app);
-export {app as adminApp , adminDb};
+export { app as adminApp, adminDb };
