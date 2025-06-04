4f55c107e72e2a1b06b4ccce444c5964a24f0f23
Talha Tayyab <qararhanzala@gmail.com>
2025-06-04 14:18:17 +0500

    new commit

--- src/app/doc/[id]/page.tsx
+++ src/app/doc/[id]/page.tsx
@@ -1,12 +1,27 @@
+// "use client";
+
+// import React, { use } from "react";
+// import Document from "@/components/Document";
+
+// function DocumentPage({ params }: { params: { id: string } }) {
+//   // Unwrap the params using React.use()
+//   const unwrappedParams = use(params);
+//   const id = unwrappedParams.id;
+
+//   return (
+//     <Document id={id} />
+//   );
+// }
+
+// export default DocumentPage;
 "use client";
 
 import React, { use } from "react";
 import Document from "@/components/Document";
 
-function DocumentPage({ params }: { params: { id: string } }) {
-  // Unwrap the params using React.use()
-  const unwrappedParams = use(params);
-  const id = unwrappedParams.id;
+function DocumentPage({ params }: { params: Promise<{ id: string }> }) {
+  // Use React.use() to unwrap the Promise
+  const { id } = use(params);
 
   return (
     <Document id={id} />
